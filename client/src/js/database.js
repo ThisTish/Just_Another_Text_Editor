ad403e520c6c13deb3d70ea746bd25864c9ec333
Tish <tishthegoat@gmail.com>
2024-07-14 05:05:36 -0600

    indexedDb fully saving

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,49 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+  // ?might need to update so there are more than one values?
 export const putDb = async (content) => {
   try {
     const contentDb = await openDB('jate', 1)
-    contentDb.transaction('jate', 'readwrite').objectStore('jate').add('content')
+    const tx = contentDb.transaction('jate', 'readwrite')
+    const store = tx.objectStore('jate')
+    const { id, ...contentWithoutId } = content
+    const req = store.add(contentWithoutId)
+    const result = await req
+    console.log('database updated', result)
   } catch (error) {
     console.log('error adding to database', error);
   }
-
-
-
-  // const contentDb = await openDB('jate', 1)
-
-  // const tx = contentDb.transaction('jate', 'readwrite')
-
-  // const store = tx.objectStore('jate')
-
-  // const req = store.add({ content})
-
-  // const result = await req
-
-  // console.log('Content Saved!', result)
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
-  const contentDb = await openDB('jate', 1)
-  contentDb.transaction('jate')
-    .objectStore('jate')
-    .getAll().onsuccess = (event) =>{
-      console.log(contentDb)
-      return event.target.result.content
-    }
-
-  // const tx = contentDb.transaction('jate', 'readonly')
-
-  // const store = tx.objectStore('jate')
-
-  // const req = store.getAll()
-
-  // const result = await req
-  // console.log('result.value', result)
-  // return JSON.stringify(result)
+  try {
+    const contentDb = await openDB('jate', 1)
+    const tx = contentDb.transaction('jate')
+    const store = tx.objectStore('jate')
+    const req = store.getAll()
+    const result = await req
+        console.log(result)
+        return result.content
+  } catch (error) {
+    console.log('Error retrieving text from database', error);
+  }
 }
 
 initdb();
